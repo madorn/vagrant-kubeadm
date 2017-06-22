81266a6c9157dae53c73fea87c1391f2ca5643e7
Matt Dorn <madorn@gmail.com>
2017-06-22 17:11:07 -0500

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,22 +1,13 @@
 #!/bin/bash -eux
-
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt-get update
-
 sudo apt-get install -y docker-ce kubelet kubeadm kubernetes-cni
-
 kubeadm reset
-
 source /vagrant/token
-
 kubeadm join --token $TOKEN 192.168.56.60:6443
